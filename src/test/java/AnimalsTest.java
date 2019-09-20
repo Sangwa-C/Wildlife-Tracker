f3cab33277af3c0c8994150b242ccf7172b7643d
Sangwa-C <chelseasabinesangwa@gmail.com>
2019-09-21 01:31:05 +0200

    tested the animal class

---
+++ src/test/java/AnimalsTest.java
@@ -0,0 +1,16 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.sql2o.*;
+
+//@Rule
+//public DatabaseRule database = new DatabaseRule();
+
+
+public class AnimalsTest {
+
+    @Test
+    public void monster_instantiatesCorrectly_true() {
+        Animals testAnimals = new Animals( 1, "dog");
+        assertEquals(true, testAnimals instanceof Animals);
+    }
+}
